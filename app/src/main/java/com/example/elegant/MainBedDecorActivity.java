f6ecfbc158a9c4d008a82c06fa20b7646c8465ea
FatimaJuairiah19 <fatimajuairiah19@gmail.com>
2020-09-23 21:09:42 +0600

    Elegant Interiors

--- app/src/main/java/com/example/elegant/MainBedDecorActivity.java
+++ app/src/main/java/com/example/elegant/MainBedDecorActivity.java
@@ -17,18 +17,18 @@ public class MainBedDecorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_bed_decor);
         bedroom = new ArrayList<>();
-        bedroom.add(new Bedroom("Classic Bedroom","Categorie Bedroom","Cost 16000Tk only",R.drawable.bedroom1));
-        bedroom.add(new Bedroom("Modern Bedroom","Categorie Bedroom","Cost 16000Tk only",R.drawable.bedroom2));
-        bedroom.add(new Bedroom("Simple Bedroom","Categorie Bedroom","Cost 15900Tk only",R.drawable.bedroom3));
-        bedroom.add(new Bedroom("Simple Bedroom","Categorie Bedroom","Cost 15900Tk only",R.drawable.bedroom4));
-        bedroom.add(new Bedroom("Classic Bedroom","Categorie Bedroom","Cost 16000Tk only",R.drawable.bedroom5));
-        bedroom.add(new Bedroom("Modern bedroom","Categorie Bedroom","Cost 15800Tk only",R.drawable.bedroom6));
-        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 16000Tk only",R.drawable.bedroom7));
-        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 16000Tk only",R.drawable.bedroom8));
-        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 15500Tk only",R.drawable.bedroom9));
-        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 16500Tk only",R.drawable.bedroom10));
-        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 15999Tk only",R.drawable.bedroom7));
-        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 16000Tk only",R.drawable.bedroom11));
+        bedroom.add(new Bedroom("Classic Bedroom","Categorie Bedroom","Cost 26000Tk only",R.drawable.bedroom1));
+        bedroom.add(new Bedroom("Modern Bedroom","Categorie Bedroom","Cost 26000Tk only",R.drawable.bedroom2));
+        bedroom.add(new Bedroom("Simple Bedroom","Categorie Bedroom","Cost 25900Tk only",R.drawable.bedroom3));
+        bedroom.add(new Bedroom("Simple Bedroom","Categorie Bedroom","Cost 25900Tk only",R.drawable.bedroom4));
+        bedroom.add(new Bedroom("Classic Bedroom","Categorie Bedroom","Cost 26000Tk only",R.drawable.bedroom5));
+        bedroom.add(new Bedroom("Modern bedroom","Categorie Bedroom","Cost 25800Tk only",R.drawable.bedroom6));
+        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 26000Tk only",R.drawable.bedroom7));
+        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 26000Tk only",R.drawable.bedroom8));
+        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 25500Tk only",R.drawable.bedroom9));
+        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 26500Tk only",R.drawable.bedroom10));
+        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 25999Tk only",R.drawable.bedroom7));
+        bedroom.add(new Bedroom("Classic bedroom","Categorie Bedroom","Cost 26000Tk only",R.drawable.bedroom11));
 
 
         RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview_id);
--- app/src/main/java/com/example/elegant/MainDrawingDecorActivity.java
+++ app/src/main/java/com/example/elegant/MainDrawingDecorActivity.java
@@ -17,18 +17,18 @@ public class MainDrawingDecorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_drawing_decor);
         bedroom = new ArrayList<>();
-        bedroom.add(new Bedroom("LIVING ROOM #01","Categorie LivingRoom","Cost 16000Tk only",R.drawable.drawingroom1));
-        bedroom.add(new Bedroom("LIVING ROOM #02","Categorie LivingRoom","Cost 16000Tk only",R.drawable.drawingroom2jpg));
-        bedroom.add(new Bedroom("LIVING ROOM #03","Categorie LivingRoom","Cost 15900Tk only",R.drawable.drawingroom3));
-        bedroom.add(new Bedroom("LIVING ROOM #04","Categorie LivingRoom","Cost 15900Tk only",R.drawable.drawingroom4));
-        bedroom.add(new Bedroom("LIVING ROOM #05","Categorie LivingRoom","Cost 16000Tk only",R.drawable.drawingroom5));
-        bedroom.add(new Bedroom("LIVING ROOM #06","Categorie LivingRoom","Cost 15800Tk only",R.drawable.drawingroom6));
-        bedroom.add(new Bedroom("LIVING ROOM #07","Categorie LivingRoom","Cost 16000Tk only",R.drawable.drawingroom7));
-        bedroom.add(new Bedroom("LIVING ROOM #08","Categorie LivingRoom","Cost 16000Tk only",R.drawable.drawingroom3));
-        bedroom.add(new Bedroom("LIVING ROOM #09","Categorie LivingRoom","Cost 15500Tk only",R.drawable.drawingroom5));
-        bedroom.add(new Bedroom("LIVING ROOM #10","Categorie LivingRoom","Cost 16500Tk only",R.drawable.drawingroom8));
-        bedroom.add(new Bedroom("LIVING ROOM #11","Categorie LivingRoom","Cost 15999Tk only",R.drawable.drawingroom9));
-        bedroom.add(new Bedroom("LIVING ROOM #12","Categorie LivingRoom","Cost 16000Tk only",R.drawable.drawingroom4));
+        bedroom.add(new Bedroom("LIVING ROOM #01","Categorie LivingRoom","Cost 26000Tk only",R.drawable.drawingroom1));
+        bedroom.add(new Bedroom("LIVING ROOM #02","Categorie LivingRoom","Cost 26000Tk only",R.drawable.drawingroom2jpg));
+        bedroom.add(new Bedroom("LIVING ROOM #03","Categorie LivingRoom","Cost 25900Tk only",R.drawable.drawingroom3));
+        bedroom.add(new Bedroom("LIVING ROOM #04","Categorie LivingRoom","Cost 25900Tk only",R.drawable.drawingroom4));
+        bedroom.add(new Bedroom("LIVING ROOM #05","Categorie LivingRoom","Cost 26000Tk only",R.drawable.drawingroom5));
+        bedroom.add(new Bedroom("LIVING ROOM #06","Categorie LivingRoom","Cost 25800Tk only",R.drawable.drawingroom6));
+        bedroom.add(new Bedroom("LIVING ROOM #07","Categorie LivingRoom","Cost 26000Tk only",R.drawable.drawingroom7));
+        bedroom.add(new Bedroom("LIVING ROOM #08","Categorie LivingRoom","Cost 26000Tk only",R.drawable.drawingroom3));
+        bedroom.add(new Bedroom("LIVING ROOM #09","Categorie LivingRoom","Cost 25500Tk only",R.drawable.drawingroom5));
+        bedroom.add(new Bedroom("LIVING ROOM #10","Categorie LivingRoom","Cost 26500Tk only",R.drawable.drawingroom8));
+        bedroom.add(new Bedroom("LIVING ROOM #11","Categorie LivingRoom","Cost 25999Tk only",R.drawable.drawingroom9));
+        bedroom.add(new Bedroom("LIVING ROOM #12","Categorie LivingRoom","Cost 26000Tk only",R.drawable.drawingroom4));
 
 
         RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview_id);
